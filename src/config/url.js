bc17022a3d1ef8e840c3e16a78633e7227f37e05
Jav1erRam0s <ramos.aranibar.javier@gmail.com>
2023-06-24 19:04:37 -0300

    update url

--- src/config/url.js
+++ src/config/url.js
@@ -1,5 +1,5 @@
 const url = {
-  path: "https://raw.githubusercontent.com/Jav1erRam0s/jimena-pintos/main",
+  path: "https://raw.githubusercontent.com/Jav1erRam0s/JSON.API-Ke.Reposteria/main",
 };
 
 export default url;
